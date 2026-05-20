cef3d910335f43a06ddbb75933b07e9fa2bd1ae0
hophnoph <collectablles@protonmail.com>
2026-05-20 14:52:42 +0100

    created a simple m3u playlist file, maker

---
+++ scripts/playlist-maker.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -e
+
+case "$1" in
+  "-h" | "--help")
+cat <<EOF
+Playlist Maker | playlist-maker.sh [args] 
+
+a simple script made to make an m3u playlist file
+depending on the media that exists in the Current Working Directory
+
+Arguments
+  --help      or -h | print this help
+  --start     or -s | start making a playlist
+  --directory or -d | specify a target direcotry
+EOF
+    ;;
+
+  "--start" | "-s")
+    echo -n "Name your Playlist: "
+    read -r playlist_name
+
+    touch "${playlist_name}".m3u
+
+    cwd_files_n_dirs=( *)
+    cwd_files=()
+
+    for i in "${cwd_files_n_dirs[@]}"; do
+      if [[ -f "$i" ]]; then
+        cwd_files+=("$i")
+      fi
+    done
+
+    for i in "${cwd_files[@]}"; do
+      echo "$i" >> "${playlist_name}".m3u
+    done    
+    ;;
+
+  "--directory" | "-d")
+    echo -n "target directory ? (don't end it with a forward slash '/'): "
+    read -r target_dir
+
+    echo
+    echo -n "Name your Playlist: "
+    read -r playlist_name
+
+    touch "${playlist_name}".m3u
+
+    target_files_n_dirs=( "$target_dir"/* )
+    target_files=()
+
+    for i in "${target_files_n_dirs[@]}"; do
+      if [[ -f "$i" ]]; then
+        target_files+=("$i")
+      fi
+    done
+
+    for i in "${target_files[@]}"; do
+      echo "$i" >> "${playlist_name}".m3u
+    done    
+    ;;
+
+  *)
+  if [[ "$1" == "" ]]; then
+    echo "YOU did not specify a command ?"
+  else
+    echo "YOUR command wasn't found !"
+  fi
+    ;;
+esac
