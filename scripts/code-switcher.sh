3de6d5d84807ea4a9b3d3a3abd2e7d8e621aafbf
hophnoph <collectablles@protonmail.com>
2026-06-09 16:54:10 +0100

    made script better logiced ?

--- scripts/code-switcher.sh
+++ scripts/code-switcher.sh
@@ -1,9 +1,9 @@
-code_project_dir="$HOME/Documents/REPO/"
+code_projects_dir="$HOME/Documents/REPO/"
 
-cd $code_project_dir
+cd "$code_projects_dir" || echo "something went wrong while switching cwd with 'cd' command" && exit
 
 gotodir=$(ls | fzf)
 
 if [[ -d "./$gotodir" ]]; then
-  cd $gotodir
-fi
+  cd "$gotodir" || echo "something went wrong while switching cwd with 'cd' command" && exit
+ fi
